f377e3caedf041d369743b79fe5060e67789b23e
Douglas0625 <douglasleon.dev@gmail.com>
2026-09-18 21:42:09 -0600

    primer version prueba con flask y jinja

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard_admin")
+def dashboard():
+    return render_template("dashboard_admin.html")
+
+
+@app.route("/propietarios")
+def propietarios():
+    return render_template("propietarios.html")
+
+
+@app.route("/inquilinos")
+def inquilinos():
+    return render_template("inquilinos.html")
+
+
+@app.route("/propiedades")
+def propiedades():
+    return render_template("propiedades.html")
+
+
+@app.route("/contratos")
+def contratos():
+    return render_template("contratos.html")
+
+
+@app.route("/pagos")
+def pagos():
+    return render_template("pagos.html")
+
+
+@app.route("/mantenimientos")
+def mantenimientos():
+    return render_template("mantenimientos.html")
+
+
+@app.route("/tecnicos")
+def tecnicos():
+    return render_template("tecnicos.html")
+
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
