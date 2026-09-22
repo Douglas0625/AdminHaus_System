f8ac55372d9d9939aa55d7061c855e3834c4aad2
Douglas0625 <douglasleon.dev@gmail.com>
2026-09-22 11:05:18 -0600

    conexion a base de datos en config con variables env

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
-app = Flask(__name__)
+from config import Config
+
+
+db = SQLAlchemy()
+
+
+app = Flask(
+    __name__,
+    template_folder="app/templates",
+    static_folder="app/static"
+)
+
+app.config.from_object(Config)
+
+db.init_app(app)
 
 
 @app.route("/")
@@ -12,13 +28,16 @@ def login():
 def dashboard_admin():
     return render_template("dashboard_admin.html")
 
+
 @app.route("/dashboard_gestor")
 def dashboard_gestor():
     return render_template("dashboard_gestor.html")
 
+
 @app.route("/dashboard_propietario")
 def dashboard_propietario():
     return render_template("dashboard_propietario.html")
+
 
 @app.route("/propietarios")
 def propietarios():
@@ -50,22 +69,33 @@ def mantenimientos():
     return render_template("mantenimientos.html")
 
 
-@app.route("/tecnicos")
-def tecnicos():
-    return render_template("tecnicos.html")
+@app.route("/proveedores")
+def proveedores():
+    return render_template("proveedores.html")
 
 
 @app.route("/usuarios")
 def usuarios():
     return render_template("usuarios.html")
 
+
 @app.route("/propietarios/nuevo", methods=["GET", "POST"])
 def nuevo_propietario():
     return render_template("nuevo_propietario.html")
 
+
 @app.route("/inquilinos/nuevo", methods=["GET", "POST"])
 def nuevo_inquilino():
     return render_template("nuevo_inquilino.html")
+
+
+@app.route("/test-db")
+def test_db():
+    try:
+        db.session.execute(text("SELECT 1"))
+        return "Conexión a PostgreSQL exitosa."
+    except Exception as e:
+        return f"Error de conexión: {e}"
 
 
 if __name__ == "__main__":
